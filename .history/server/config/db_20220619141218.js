17a2098b1da74a9a96d89a0d049e628362285520
JULIENIS BRINEZ <julebrz@gmail.com>
2022-06-19 14:57:16 -0400

    modules update

---
+++ .history/server/config/db_20220619141218.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "Module", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb:";
+exports.RemoteURI = process.env.RemoteURI;
+exports.HostName = "MongoDB Atlas";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
